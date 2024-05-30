05ff894361be62152d07a73d022c2c6a88c5385a
Moses mpande <www.mosesmpande@gmail.com>
2024-05-30 14:41:11 +0200

    my commit

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,9 +4,8 @@ import 'package:app_repo/app_repo.dart';
 import 'package:bicycle_app/app/view/app.dart';
 import 'package:bicycle_app/bootstrap.dart';
 import 'package:bicycle_app/config.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-
-
 
 class MyHttpOverrides extends HttpOverrides {
   @override
@@ -17,10 +16,19 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-void main() {
-    WidgetsFlutterBinding.ensureInitialized();
-  bootstrap(() async => App(await AppRepo.init(config: 
-        kDevConfig,
-      ),),);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data =
+      await PlatformAssetBundle().load('assets/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+  await bootstrap(
+    () async => App(
+      await AppRepo.init(
+        config: kDevConfig,
+      ),
+    ),
+  );
   HttpOverrides.global = MyHttpOverrides();
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,12 @@
+
+
 import 'dart:io';
 
 import 'package:app_repo/app_repo.dart';
 import 'package:bicycle_app/app/view/app.dart';
 import 'package:bicycle_app/bootstrap.dart';
 import 'package:bicycle_app/config.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 
@@ -17,9 +20,12 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-void main() {
+void main()async {
     WidgetsFlutterBinding.ensureInitialized();
-  bootstrap(() async => App(await AppRepo.init(config: 
+
+  ByteData data = await PlatformAssetBundle().load('assets/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+  await bootstrap(() async => App(await AppRepo.init(config: 
         kDevConfig,
       ),),);
   HttpOverrides.global = MyHttpOverrides();
